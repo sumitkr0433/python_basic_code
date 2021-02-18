e05a5c3742359c3f20113e28915748dbe10286e2
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2021-02-18 14:07:05 +0530

    Update 1.Local-Global_variable.py

--- variables/1.Local-Global_variable.py
+++ variables/1.Local-Global_variable.py
@@ -1,4 +1,4 @@
-# Local versus Global
+#1..Local versus Global
 
 # we define a function, called local
 def local():
@@ -10,3 +10,16 @@ print(m) this will print the value of m
 
 # we call, or `execute` the function local
 local() #this will print the value of variable m which is defined in the function
+####################################################################################################################################################
+#2nd variable Local versus Global
+
+def local():
+    # m doesn't belong to the scope defined by the local function
+    # so Python will keep looking into the next enclosing scope.
+    # m is finally found in the global scope
+    print(m, 'printing from the local scope')
+
+m = 5
+print(m, 'printing from the global scope')
+
+local()
