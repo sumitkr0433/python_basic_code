c6937e7e41369a9e265ae3849f5bcf71baaa9462
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2021-02-18 12:43:51 +0530

    Create 2.factorial_program.py

---
+++ variables/2.factorial_program.py
@@ -0,0 +1,7 @@
+#1...module based program
+from math import factorial
+factorial(5)
+#output we are getting
+120
+
+################################################################################################################
