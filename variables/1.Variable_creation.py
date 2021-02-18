df4f94660789983c585cc958d332c3c82730373b
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2021-02-18 12:57:54 +0530

    Create 1.Variable_creation.py

---
+++ variables/1.Variable_creation.py
@@ -0,0 +1,29 @@
+##Varible creation 
+n = 3  # integer number
+>>> address = "221b Baker Street, NW1 6XE, London"  # Sherlock Holmes' address
+>>> employee = {
+...     'age': 45,
+...     'role': 'CTO',
+...     'SSN': 'AB1234567',
+... }
+
+
+>>> # let's print them
+>>> n
+3
+
+
+>>> address
+'221b Baker Street, NW1 6XE, London'
+
+>>> employee
+{'age': 45, 'role': 'CTO', 'SSN': 'AB1234567'}
+
+
+
+#if variable is not craeted then we get this error
+>>> other_name
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'other_name' is not defined
+>>>
