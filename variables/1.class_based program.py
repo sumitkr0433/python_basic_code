ad15ac3fff399fbf9dce250286bb4ae521603a37
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2021-02-18 12:34:51 +0530

    Update and rename 1.program.py to 1.class_based program.py

---
+++ variables/1.class_based program.py
@@ -0,0 +1,35 @@
+# let's define the class Bike
+class Bike:
+
+	#construction creation
+	#taking three arguments one is iteself object and other two are the value which we passing at the time of initialising variable
+    def __init__(self, colour, frame_material):
+        self.colour = colour
+        self.frame_material = frame_material
+    
+    #creating the another function which will print the message
+    def brake(self):
+        print("Braking!")
+
+# let's create a couple of instances and sending value to the constructor 
+red_bike = Bike('Red', 'Carbon fiber')
+blue_bike = Bike('Blue', 'Steel')
+
+# let's inspect the objects we have, instances of the Bike class.
+##now calling the initialsing variable in constructor on the basis of object
+print(red_bike.colour)  # prints: Red
+print(red_bike.frame_material)  # prints: Carbon fiber
+
+
+###on the basis of the blue object acccesing the constructor variable
+print(blue_bike.colour)  # prints: Blue
+print(blue_bike.frame_material)  # prints: Steel
+
+#calling the another function
+# let's brake!
+red_bike.brake()  # prints: Braking!
+
+
+##########################################################################################################################################################
+#Interview Question:---
+#Create an class and initialize the value during the object creation and calling that function and create an another function based on the object and called it?
